632d965ede5addb53d23dc8e40bc080bc46ef720
Bruno Martins <bgcngm@gmail.com>
2023-06-30 01:34:20 +0100

    hal: Include ipc headers from agm and pal
    Change-Id: I80d59fe48f59a6700fcad87becde6639d28c843b

--- hal/Android.mk
+++ hal/Android.mk
@@ -56,7 +56,8 @@ LOCAL_C_INCLUDES += \
     vendor/qcom/opensource/core-utils/fwk-detect \
     vendor/qcom/opensource/pal \
     $(call include-path-for, audio-effects) \
-    $(LOCAL_PATH)/audio_extn
+    $(LOCAL_PATH)/audio_extn \
+    $(TOP)/vendor/qcom/opensource/pal/ipc/HwBinders/pal_ipc_server/inc/
 
 LOCAL_SRC_FILES := \
     AudioStream.cpp \
